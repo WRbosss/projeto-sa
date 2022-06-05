67f7430782f2e46a2fdf490e38c8086fa3e42d69
lucas GG <poweredlucas@gmail.com>
2022-06-05 15:55:36 -0300

    testing

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MenuPage implements OnInit {
   pages: {title:String, url:String}[] = [
     {
-      "title": "cu",
+      "title": "início",
       "url": "outro cu"
     }
   ]
