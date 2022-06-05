8bba4e1aa4016c1f672a2181df904d80c8d7f3aa
lucas GG <poweredlucas@gmail.com>
2022-06-05 15:53:04 -0300

    primeiras mudanças

---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+  pages: {title:String, url:String}[] = [
+    {
+      "title": "cu",
+      "url": "outro cu"
+    }
+  ]
+
+    
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
